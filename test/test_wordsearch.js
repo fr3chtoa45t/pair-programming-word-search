6c29b91221ca50c3b2814c3eeb344dfc1d6a1f87
Jordan Cronier <jcronier777@gmail.com>
2021-09-23 01:28:29 +0000

    Implemented a wordsearch function with Edmund Yu and Kurt Spiker

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,37 +1,80 @@
-const chai = require('chai');
+// test_wordsearch.js
+
+// In collaboration with Edmund Yu and Kurt Spiker
+
+const chai = require("chai");
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require("../wordsearch.js");
 
-describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
-    const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+describe("#wordSearch()", function () {
+  it("should return false if the word is not present", function () {
+    const grid = [
+      ["A", "W", "C", "F", "Q", "U", "A", "L"],
+      ["S", "E", "I", "N", "F", "E", "L", "D"],
+      ["Y", "F", "C", "F", "Q", "U", "A", "L"],
+      ["H", "M", "J", "T", "E", "V", "R", "G"],
+      ["W", "H", "C", "S", "Y", "E", "R", "L"],
+      ["B", "F", "R", "E", "N", "E", "Y", "B"],
+      ["U", "B", "T", "W", "A", "P", "A", "I"],
+      ["O", "D", "C", "A", "K", "U", "A", "S"],
+      ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+    ];
 
-    assert.isFalse(result);
+    const result = wordSearch(grid, "UQF");
+
+    assert.isTrue(result);
   });
 
-  it("should return true if the word is present", function() {
-    const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+  it("should return true if the word is present", function () {
+    const grid = [
+      ["A", "W", "C", "F", "Q", "U", "A", "L"],
+      ["S", "E", "I", "N", "F", "E", "L", "D"],
+      ["Y", "F", "C", "F", "Q", "U", "A", "L"],
+      ["H", "M", "J", "T", "E", "V", "R", "G"],
+      ["W", "H", "C", "S", "Y", "E", "R", "L"],
+      ["B", "F", "R", "E", "N", "E", "Y", "B"],
+      ["U", "B", "T", "W", "A", "P", "A", "I"],
+      ["O", "D", "C", "A", "K", "U", "A", "S"],
+      ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+    ];
+
+    const result = wordSearch(grid, "SEINFELD");
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present", function () {
+    const grid = [
+      ["A", "W", "C", "F", "Q", "U", "A", "L"],
+      ["S", "E", "I", "N", "F", "E", "L", "D"],
+      ["Y", "F", "C", "F", "Q", "U", "A", "L"],
+      ["H", "M", "J", "T", "E", "V", "R", "G"],
+      ["W", "H", "C", "S", "Y", "E", "R", "L"],
+      ["B", "F", "R", "E", "N", "E", "Y", "B"],
+      ["U", "B", "T", "W", "A", "P", "A", "I"],
+      ["O", "D", "C", "A", "K", "U", "A", "S"],
+      ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+    ];
+
+    const result = wordSearch(grid, "LARRY");
+
+    assert.isTrue(result);
+  });
+  it("should return true if the word is present", function () {
+    const grid = [
+      ["A", "W", "C", "F", "Q", "U", "A", "L"],
+      ["S", "E", "I", "N", "F", "E", "L", "D"],
+      ["Y", "F", "L", "F", "Q", "U", "A", "L"],
+      ["H", "M", "J", "O", "E", "V", "R", "G"],
+      ["W", "H", "C", "S", "L", "E", "R", "L"],
+      ["B", "F", "R", "E", "N", "E", "Y", "B"],
+      ["U", "B", "T", "W", "A", "P", "A", "I"],
+      ["O", "D", "C", "A", "K", "U", "A", "S"],
+      ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+    ];
+
+    const result = wordSearch(grid, "LOL");
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,64 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// wordsearch.js
 
-module.exports = wordSearch
+// In collaboration with Edmund Yu and Kurt Spiker
+
+const transpose = function (matrix) {
+  let newArr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i === 0) {
+        newArr.push([matrix[i][j]]);
+      } else {
+        newArr[j].push(matrix[i][j]);
+      }
+    }
+  }
+  return newArr;
+};
+
+const wordSearch = (letters, word) => {
+  const wordObject = {
+    regular: letters.map((ls) => ls.join("")),
+    transpose: transpose(letters).map((ls) => ls.join("")),
+    reverseRegular: letters.map((ls) => ls.reverse().join("")),
+    reverseTranspose: transpose(letters).map((ls) => ls.reverse().join("")),
+
+    findWord: function (grid, word) {
+      for (l of grid) {
+        if (l.includes(word)) return true;
+      }
+      return false;
+    },
+
+    diagonal: function (letters, word) {
+      for (let i = 0; i < letters.length; i++) {
+        for (let j = 0; j < letters[i].length; j++) {
+          if (letters[i][j] === word[0]) {
+            let lettersFound = 0;
+            for (let x = 1; x < word.length; x++) {
+              if (x + i < letters.length && x + j < letters[i].length) {
+                if (letters[i + x][j + x] === word[x]) lettersFound++; 
+              }
+            }
+            if (lettersFound === word.length - 1) return true;
+          }
+        }
+      }
+      return false;
+    },
+  };
+
+  if (
+    Object.keys(wordObject)
+      .slice(0, 4)
+      .find(
+        (key) =>
+          wordObject.findWord(wordObject[key], word) ||
+          wordObject.diagonal(wordObject[key], word)
+      )
+  ) return true;
+
+  return false;
+};
+
+module.exports = wordSearch;
